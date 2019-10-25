2afb10fa7eb579f9f189119959947a3885709d88
klubba575 <wilderandrew9@gmail.com>
2019-10-24 21:58:15 -0400

    Lab 10, Movie List Using Add Class

---
+++ Movie List - Lab 10/Movie.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movie_List___Lab_10
+{	
+	//Movie - Title(string), Genre(string)
+	class Movie
+	{
+		//fields
+		private string title;
+		private string genre;
+
+		//properties
+		#region properties
+		public string Title
+		{
+			get { return title; }
+			set { title = value; }
+		}
+		public string Genre
+		{
+			get { return genre; }
+			set { genre = value; }
+		}
+		#endregion
+		public Movie()
+		{
+
+		}
+		public Movie(string _title, string _genre)
+		{
+			title = _title;
+			genre = _genre;
+		}
+		public static List<string> GetMovieGenres(List<Movie> movies)
+		{
+			List<string> movieGenres = new List<string>();
+				
+				foreach(Movie movie in movies)
+				{
+					if (!movieGenres.Contains(movie.Genre))
+				{
+					movieGenres.Add(movie.Genre);
+				}
+			}
+			return movieGenres;
+		}
+		public static void PrintMovieGenres(List<Movie> movieList)
+		{
+			List<string> movieGenres = Movie.GetMovieGenres(movieList);
+			for (int i = 0; i < movieGenres.Count; i++)
+			{
+				Console.WriteLine($"{movieGenres[i]}");
+			}
+		}
+		public static void PrintMovieByType(List<Movie> movieList, string selection)
+		{
+			for (int i = 0; i < movieList.Count; i++)
+			{
+				if (selection == movieList[i].Genre)
+				{
+					Console.WriteLine($"{movieList[i].Title} is a {movieList[i].Genre}");
+				}
+			}
+
+		}
+	}
+}
---
+++ Movie List - Lab 10/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Movie_List___Lab_10
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Creating a new List called movieList
+			List<Movie> movieList = new List<Movie>
+			{
+				//Adding Titles and Genres to the  Class Movie, notice syntax
+				new Movie("The Thin Red Line", "Drama"),
+				new Movie("Charlie and the Chocolate Factory", "Comedy"),
+				new Movie("SemiPro", "Comedy"),
+				new Movie("DieHard With a Vengeance", "Action"),
+				new Movie("John Wick 2", "Action"),
+				new Movie("Good Will Hunting", "Drama"),
+				new Movie("The Empire Strikes Back", "Sci-Fi"),
+				new Movie("Blade Runner", "Sci-Fi"),
+				new Movie("Between Two Ferns", "Comedy"),
+				new Movie("Clear and Present Danger", "Action"),
+				new Movie("It's A Wonderful Life", "Drama"),
+				new Movie("Westworld", "Sci-Fi")
+			};
+			bool goAgain = true;
+			do
+			{
+				Console.WriteLine("What kind of movie would you like to see?");
+
+				Movie.PrintMovieGenres(movieList);
+
+				string selection = Console.ReadLine();
+
+				Movie.PrintMovieByType(movieList, selection);
+				//Ask the User if they want to go again.
+				string repeat = GetUserInput($"Would you like to select another genre? (y/n)");
+				if (repeat == "y")
+				{
+					goAgain = true;
+				}
+				else
+				{
+					goAgain = false;
+				}
+			}
+			while (goAgain);
+			Console.WriteLine("Hope you enjoy your movie!");
+		}
+		public static string GetUserInput(string message)
+		{
+			Console.WriteLine(message);
+			string input = Console.ReadLine();
+			return input;
+		}
+	}
+}
